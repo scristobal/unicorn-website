28e4a7c3a531c01ba244fd1a231eea807f08282b
Samuel Cristobal <el.tobal@gmail.com>
2023-05-15 19:11:29 +0200

    profiles

---
+++ TODOS.md
@@ -0,0 +1,10 @@
+# TODO tracking
+
+filter articles
+
+- by publication date?
+- a highlighted tag?
+
+show all members?
+problematic on smaller devices, need to scroll to get to the news
+use a carrousel 3 ppl on larger devices, one person on smaller?
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 // 1. Import utilities from `astro:content`
 import { z, defineCollection } from 'astro:content';
 // 2. Define your collection(s)
-const people = defineCollection({
+const team = defineCollection({
     schema: ({ image }) =>
         z.object({
             name: z.string(),
@@ -11,14 +11,16 @@ const people = defineCollection({
         })
 });
 
-const news = defineCollection({
+export const articles = defineCollection({
     schema: z.object({
-        heading: z.string()
+        heading: z.string(),
+        date: z.string(),
+        summary: z.string()
     })
 });
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-    people,
-    news
+    team,
+    articles
 };
