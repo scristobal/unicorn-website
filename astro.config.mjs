75d210f967aee02cf923873c164bf962ad6645bb
Samuel Cristobal <el.tobal@gmail.com>
2023-09-02 10:30:46 +0200

    update to astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig, sharpImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({ config: { applyBaseStyles: false } }), mdx()],
-    experimental: {
-        assets: true
-    },
-    image: {
-        service: sharpImageService()
-    }
+    integrations: [tailwind({ config: { applyBaseStyles: true } }), mdx()],
+    experimental: { },
+    image: { service: sharpImageService() }
 });
