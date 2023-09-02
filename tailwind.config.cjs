fb1ca577bfdce58f858460c0480cea64b3a339d0
Samuel Cristobal <el.tobal@gmail.com>
2023-09-02 21:26:31 +0200

    added fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,15 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {},
+    theme: {
+        extend: {
+            fontFamily: {
+                alegreya: 'alegreya-sc, serif',
+                raleway: 'raleway, sans-serif',
+                aleo: 'aleo, serif',
+                merryweather: 'Merriweather, serif'
+            }
+        }
+    },
     plugins: [require('@tailwindcss/typography')]
 };
