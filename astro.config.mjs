2e37d887b406fd182a778a677ea15615f6f9d6c3
Samuel Cristobal <el.tobal@gmail.com>
2023-09-03 09:27:17 +0200

    custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://scristobal.github.io',
-    base: '/unicorn-website',
     integrations: [tailwind({ config: { applyBaseStyles: true } }), mdx()],
     experimental: {},
     image: { service: sharpImageService() }
