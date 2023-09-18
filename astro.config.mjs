96cd05450b06539cb9be672c1bc7db59ca163e3f
Samuel Cristobal <el.tobal@gmail.com>
2023-09-18 20:34:26 +0200

    added prefetch plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig, sharpImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({ config: { applyBaseStyles: true } }), mdx()],
+    integrations: [tailwind({ config: { applyBaseStyles: true } }), mdx(), prefetch({ throttle: 3 })],
     experimental: {},
     image: { service: sharpImageService() }
 });
