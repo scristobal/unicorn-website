65a24f71568220e7ab7a951ae0fcd40ad7590add
Samuel Cristobal <el.tobal@gmail.com>
2023-09-19 07:59:19 +0200

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,26 @@ import { defineConfig, sharpImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import prefetch from '@astrojs/prefetch';
+import sitemap from '@astrojs/sitemap';
+import { SITE_URL } from './const';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({ config: { applyBaseStyles: true } }), mdx(), prefetch({ throttle: 3 })],
+    site: SITE_URL,
+    integrations: [
+        tailwind({
+            config: {
+                applyBaseStyles: true
+            }
+        }),
+        mdx(),
+        prefetch({
+            throttle: 3
+        }),
+        sitemap()
+    ],
     experimental: {},
-    image: { service: sharpImageService() }
+    image: {
+        service: sharpImageService()
+    }
 });
---
+++ const.ts
@@ -0,0 +1,1 @@
+export const SITE_URL = 'https://unicorn.messy.page/';
